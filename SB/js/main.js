d8d1cbfb47d2e33794de46f05040fc2aafca1531
leo9045 <ayuship14plus@gmail.com>
2026-01-14 19:06:27 +0530

    Update main.js

--- SB/js/main.js
+++ SB/js/main.js
@@ -1,1 +1,12 @@
-console.log("Suraj Band Elegant Theme Loaded");
+const burger = document.getElementById("burger");
+const navMenu = document.getElementById("navMenu");
+
+burger.addEventListener("click", ()=>{
+  navMenu.classList.toggle("show");
+});
+
+navMenu.querySelectorAll("a").forEach(link=>{
+  link.addEventListener("click", ()=>{
+    navMenu.classList.remove("show");
+  });
+});
